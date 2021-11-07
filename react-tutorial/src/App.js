bbe93da302f2e9922dd221579e9bb72882b8c448
vinzmc <maurice.marvin@student.umn.ac.id>
2021-11-07 18:13:56 +0700

    react tutorial from taniarascia

---
+++ react-tutorial/src/App.js
@@ -0,0 +1,44 @@
+//dependency
+import React from 'react'
+
+//css
+import './App.css';
+
+//component
+import Table from './Table'
+import Form from './Form'
+import Api from './Api'
+
+class App extends React.Component {
+  state = {
+    characters: [],
+  }
+
+  removeCharacter = (index) => {
+    const { characters } = this.state
+
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index
+      }),
+    })
+  }
+
+  handleSubmit = (character) => {
+    this.setState({ characters: [...this.state.characters, character] })
+  }
+
+  render() {
+    const { characters } = this.state;
+
+    return (
+      <div className="container">
+        <Table characterData={characters} removeCharacter={this.removeCharacter} />
+        <Form handleSubmit={this.handleSubmit} />
+        <Api />
+      </div>
+    )
+  }
+}
+
+export default App;
